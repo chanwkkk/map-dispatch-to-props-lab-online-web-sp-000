b23369a63a40e8f6d0e2db4212c85c465b582ea5
Yingqi Chen <chanwk18@hotmail.com>
2020-02-28 07:29:44 +0000

    Automatically backed up by Learn

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -23,6 +23,7 @@ export class RestaurantInput extends Component {
 
   handleOnSubmit = event => {
     event.preventDefault();
+    this.props.addRestaurant()
     // add missing code
   }
 
@@ -49,6 +50,13 @@ export class RestaurantInput extends Component {
   }
 };
 
+const mapStateToProps = state => {
+  return {restaurants: state.restaurants}
+}
+const mapDispatchToProps = dispatch=>{
+  return {addRestaurant: ()=>dispatch(addRestaurant())}
+}
+
 
 //connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+export default connect(mapStateToProps,mapDispatchToProps)(RestaurantInput)
